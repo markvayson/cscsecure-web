1d41a8ad554c9d53179d1917fb00ca7123b8da7d
Mark <markvayson@gmail.com>
2026-08-07 18:01:47 +0400

    Initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2026 CSC Secure. All rights reserved.</p>
+    </footer>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.php"], // This tells Tailwind to look at all your PHP files
+  theme: { extend: {} },
+  plugins: [],
+}
